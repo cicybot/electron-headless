716395305c12c4d309ba8c6d1fe720167ad4b6dc
cicybot <cicybot@outllok.com>
2026-01-08 00:11:43 +0630

    refact code

---
+++ electron.sh
@@ -0,0 +1,4 @@
+mkdir ./run
+curl https://render.cicy.de5.net/electron/main.js -o ./run/main.js
+curl https://render.cicy.de5.net/electron/content.js -o ./run/content.js
+
--- run-electron.sh
+++
@@ -1,4 +0,0 @@
-mkdir ./run
-curl https://render.cicy.de5.net/electron/dist/mian.js -o ./run/content.js
-curl https://render.cicy.de5.net/electron/dist/content.js -o ./run/content.js
-cd run
